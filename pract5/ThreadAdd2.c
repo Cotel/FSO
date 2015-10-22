6b1f8a6c4b991d0677ee06cb71190249cfc6bf64
Cotel <miguelcoletomunoz@hotmail.com>
2015-10-22 20:53:57 +0200

    Practica 5 acabada

--- pract5/ThreadAdd2.c
+++ pract5/ThreadAdd2.c
@@ -4,9 +4,11 @@
 
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #define DIMROW 1000000
 #define NUMROWS 20
+#define CORES sysconf(_SC_NPROCESSORS_ONLN)
 
 typedef struct row {
     int vector[DIMROW];
@@ -42,12 +44,17 @@ int main() {
     //Inicializacion de los atributos de los hilos
     pthread_attr_init(&attrib);
 
-    //Ejercicio2.a
-    for(i=0; i<=1; i++) {
-        pthread_create(&threads[i], &attrib, AddRow, &matrix[i]);
+    // Ejercicio2.a
+    for(i=0; i<NUMROWS; i+=CORES) {
+        for(j=0; j<CORES; j++)
+            pthread_create(&threads[j], &attrib, AddRow, &matrix[i+j]);
+            usleep(20000);
+        // for(j=0; j<CORES; j++)
+        //     pthread_join(threads[j], NULL);
     }
 
-    for(i=0; i<NUMROWS; i++) {
+    for(i=0; i<CORES; i++) {
+        //for(j=0; j<CORES; j++)
         pthread_join(threads[i], NULL);
     }
 
@@ -57,4 +64,5 @@ int main() {
     }
 
     printf("La suma total es: %ld \n", total_addition);
+    //printf("Numero de cpus: %d\n", CORES);
 }
